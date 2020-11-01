13f989b27b46ec2c71d2f76d27ac055c6a3aff26
Zachary Durr <zdurr22@gmail.com>
2020-11-01 18:09:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Add your code here
+class Dog
+  attr_accessor :name,
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each do |name|
+      puts "#{@name}"
+    end
+  end
+
+  def save
+    @@all << self
+  end
+
+end
